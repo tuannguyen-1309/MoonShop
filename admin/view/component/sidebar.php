aa91cd9a78f8e92622e89c7da1ee23ae15e33a9c
tuannguyen-1309 <tuandungyn2@gmail.com>
2024-11-09 13:45:12 +0700

    phần sidebar admin

---
+++ admin/view/component/sidebar.php
@@ -0,0 +1,35 @@
+         <!-- Sidebar trái -->
+         <div class="shadow bg-light pb-5 mt-4 mb-4 col-md-2">
+            <ul class="nav nav-pills list-group" style="margin-bottom: 300px;">
+                <li class="list-group-item list-group-item-action rounded-0 border-0 mt-3">
+                    <a href="index.php">
+                        <i class="fa-solid fa-house fa-fw"></i> Dashboard
+                    </a>
+                </li>
+                <li class="list-group-item list-group-item-action rounded-0 border-0 mt-3">
+                    <a href="index.php?act=list-category">
+                        <i class="fa-solid fa-layer-group fa-fw"></i> Danh mục sản phẩm
+                    </a>
+                </li>
+                <li class="list-group-item list-group-item-action rounded-0 border-0 mt-3">
+                    <a href="index.php?act=list-product">
+                        <i class="fa-solid fa-desktop fa-fw"></i> Sản phẩm
+                    </a>
+                </li>
+                <li class="list-group-item list-group-item-action rounded-0 border-0 mt-3 mb-3">
+                    <a href="index.php?act=list-account">
+                        <i class="fa-regular fa-user"></i></i> Tài khoản
+                    </a>
+                </li>
+                <li class="list-group-item list-group-item-action rounded-0 border-0 mt-3 mb-3">
+                    <a href="index.php?act=list-bill">
+                        <i class="fa-regular fa-clipboard fa-fw"></i> Đơn hàng
+                    </a>
+                </li>
+                  <li class="list-group-item list-group-item-action rounded-0 border-0 mt-3 mb-3">
+                    <a href="index.php?act=list-billDetail">
+                        <i class="fa-regular fa-clipboard fa-fw"></i> Đơn hàng chi tiết
+                    </a>
+                </li>
+            </ul>
+        </div>

3a5acf36a1aba25545bbc06b82b16f1a5764b17b
tuannguyen-1309 <tuandungyn2@gmail.com>
2024-11-09 13:44:55 +0700

    phần header

---
+++ admin/view/component/header.php
@@ -0,0 +1,49 @@
+<header class="bg-light pb-2 pt-2">
+        <div class="container">
+            <!-- Header top -->
+            <div class="d-flex justify-content-between align-items-center pb-2">
+                <!-- Left -->
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="pe-3">
+                        <i class="fa-solid fa-phone"></i> +8412345678
+                    </div>
+                    <div>
+                        <i class="fa-solid fa-envelope"></i> tamca_k19@fpt.edu.vn
+                    </div>
+                </div>
+                <div class="d-flex justify-content-end align-items-center">
+                    <i class="fa-solid fa-user me-2"></i>  
+                     Xin chào Tam Ca K19
+                </div>
+            </div>
+            <!-- End header top -->
+            <!-- Header bottom -->
+            <div class="d-flex justify-content-between align-items-center bg-white rounded-pill mb-2">
+                <div class="d-flex align-items-center justify-content-center p-2 ms-2" >
+                    <img src="../img/logo.png" class="pe-2" alt="">
+                    <h3>MOON SHOP </h3>
+                </div>
+                <!-- MENU -->
+                <nav class="navbar navbar-expand-lg">
+                    <div class="container-fluid">
+                        <div class="navbar-nav">
+                            <a class="nav-link active" aria-current="page" href="#">Trang chủ</a>
+                            <a class="nav-link" href="#">Giới thiệu</a>
+                            <a class="nav-link" href="#">Sản phẩm</a>
+                            <a class="nav-link" href="#">Liên hệ</a>
+                        </div>
+                    </div>
+                </nav>
+                <!-- END MENU -->
+                <div class="d-flex justify-content-between align-items-center pe-2">
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2 rounded-pill" type="search" placeholder="Search" aria-label="Search">
+                    </form>
+                    <i class="fa-regular fa-heart me-2"></i>
+                    <i class="fa-solid fa-cart-shopping me-2"></i>
+                </div>
+            </div>
+            <!-- End header bottom -->
+        </div>
+    </header>   
+    <!-- END HEADER -->

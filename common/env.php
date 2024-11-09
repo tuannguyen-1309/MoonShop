510b4cbc53de32c20e71a2e0520bf1d8925a58f0
tuannguyen-1309 <tuandungyn2@gmail.com>
2024-11-09 13:46:50 +0700

    khai báo và cấu hình biến môi trường

---
+++ common/env.php
@@ -0,0 +1,11 @@
+<?php
+//Khai báo môi trường, các biến dùng chung
+//Khai báo dạng hằng số
+
+define('DB_HOST','localhost');
+define('DB_PORT','3306');
+define('DB_USER','root');
+define('DB_PASS','123456');
+define('DB_NAME','mun_pet');
+
+?>

761ddfd39f9001997c5dc4536361d7241b8534ec
tuannguyen-1309 <tuandungyn2@gmail.com>
2024-11-09 13:44:45 +0700

    phần footer

---
+++ admin/view/component/footer.php
@@ -0,0 +1,56 @@
+<!-- FOOTER -->
+<footer class="bg-light pt-5 pb-5">
+        <div class="col-md-9 container">
+            <div class="text-dark row">
+                <div class="col-md-3">
+                    <h5>VĂN PHÒNG GIAO DỊCH</h5>
+                    <ul class="list-group">
+                    <li><a class="text-secondary" href="">Trang chủ</a></li>
+                    <li><a class="text-secondary" href="">Giới thiệu</a></li>
+                    <li><a class="text-secondary" href="">Sản phẩm</a></li>
+                    <li><a class="text-secondary" href="">Tin mới nhất</a></li>
+                    <li><a class="text-secondary" href="">Câu hỏi thường gặp</a></li>
+                    <li><a class="text-secondary" href="">Tuyển dụng</a></li>
+                    <li><a class="text-secondary" href="">Liên hệ</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h5>VỀ CHÚNG TÔI</h5>
+                    <ul class="list-group">
+                    <li><a class="text-secondary" href="">Trang chủ</a></li>
+                    <li><a class="text-secondary" href="">Giới thiệu</a></li>
+                    <li><a class="text-secondary" href="">Sản phẩm</a></li>
+                    <li><a class="text-secondary" href="">Tin mới nhất</a></li>
+                    <li><a class="text-secondary" href="">Câu hỏi thường gặp</a></li>
+                    <li><a class="text-secondary" href="">Tuyển dụng</a></li>
+                    <li><a class="text-secondary" href="">Liên hệ</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h5>CHÍNH SÁCH BÁN HÀNG</h5>
+                    <ul class="list-group">
+                    <li><a class="text-secondary" href="">Trang chủ</a></li>
+                    <li><a class="text-secondary" href="">Giới thiệu</a></li>
+                    <li><a class="text-secondary" href="">Sản phẩm</a></li>
+                    <li><a class="text-secondary" href="">Tin mới nhất</a></li>
+                    <li><a class="text-secondary" href="">Câu hỏi thường gặp</a></li>
+                    <li><a class="text-secondary" href="">Tuyển dụng</a></li>
+                    <li><a class="text-secondary" href="">Liên hệ</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h5>THEO DÕI CHÚNG TÔI</h5>
+                    <ul class="list-group">
+                    <li><a class="text-secondary" href="">Trang chủ</a></li>
+                    <li><a class="text-secondary" href="">Giới thiệu</a></li>
+                    <li><a class="text-secondary" href="">Sản phẩm</a></li>
+                    <li><a class="text-secondary" href="">Tin mới nhất</a></li>
+                    <li><a class="text-secondary" href="">Câu hỏi thường gặp</a></li>
+                    <li><a class="text-secondary" href="">Tuyển dụng</a></li>
+                    <li><a class="text-secondary" href="">Liên hệ</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- END FOOTER -->

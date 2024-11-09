2e03d7c8b93e474f6489565abcc4405ee77dc65f
tuannguyen-1309 <tuandungyn2@gmail.com>
2024-11-09 13:41:40 +0700

    tạo lớp product

---
+++ admin/model/product.php
@@ -0,0 +1,14 @@
+<?php
+    Class Product {
+        public $pro_id;
+        public $pro_name;
+        public $pro_price;
+        public $pro_quantity;
+        public $pro_description;
+        public $pro_img;
+        public $pro_status;
+        public $cate_id;
+        public $cate_name;
+    }
+
+?>

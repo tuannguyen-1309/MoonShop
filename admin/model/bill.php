5f82cc533e1b14675221a4d96ef27686655b9549
tuannguyen-1309 <tuandungyn2@gmail.com>
2024-11-09 13:40:05 +0700

    tạo các lớp bill và bill_detail

---
+++ admin/model/bill.php
@@ -0,0 +1,16 @@
+<?php
+    class Bill {
+        public $bill_id;
+        public $account_id;
+        public $account_name;
+
+    }
+
+    class BillDetail {
+        public $bill_detail_id;
+        public $bill_id;
+        public $pro_id;
+        public $pro_name;
+        
+    }
+?>
